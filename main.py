4af33807fc22e339f833e251f0020caf61b0b17c
TheCodeDiver <90726868+TheCodeDiver@users.noreply.github.com>
2025-07-04 14:17:57 -0400

    added collision

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -34,6 +35,12 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)   
+
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                print("Game Over!")
+                sys.exit()
+
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
